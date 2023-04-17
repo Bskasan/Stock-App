4d15e966c4d3d0a4546bbc732f5e7b666ae01f37
Bskasan <b.kasan@yandex.com>
2023-04-17 21:32:22 +0300

    firms bug fixed finally

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice"
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import { createStore } from "react-redux";
 
@@ -24,6 +25,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock: stockReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
