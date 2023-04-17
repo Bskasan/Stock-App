76661258538ea490caa5f3471e47c8ec880441af
Bskasan <b.kasan@yandex.com>
2023-04-18 01:17:59 +0300

    products 1

---
+++ src/components/modals/ProductModal.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
+import { modalStyle } from "../../styles/globalStyles";
+import { useState } from "react";
+import useStockCall from "../../hooks/useStockCall";
+
+export default function ProductModal({ open, handleClose, info, setInfo }) {
+  //   const [info, setInfo] = useState({
+  //     name: "",
+  //     phone: "",
+  //     address: "",
+  //     image: "",
+  //   });
+
+  const { postStockData, putStockData } = useStockCall();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInfo({ ...info, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (info.id) {
+      putStockData("products", info);
+    } else {
+      postStockData("products", info);
+    }
+
+    handleClose();
+    setInfo({ name: "", phone: "", address: "", image: "" });
+  };
+
+  console.log(info);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => {
+          handleClose();
+          setInfo({ name: "", phone: "", address: "", image: "" });
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component="form"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              required
+              value={info?.name}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Phone"
+              name="phone"
+              id="phone"
+              type="tel"
+              variant="outlined"
+              required
+              value={info?.phone}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Address"
+              name="address"
+              id="address"
+              type="text"
+              variant="outlined"
+              required
+              value={info?.address}
+              onChange={handleChange}
+            />
+
+            <TextField
+              label="Image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              required
+              value={info?.image}
+              onChange={handleChange}
+            />
+
+            <Button type="submit" variant="contained">
+              Submit Firm
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,65 @@
+import { Button, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import useStockCall from "../hooks/useStockCall";
+import { flex } from "../styles/globalStyles";
+import ProductModal from "../components/modals/ProductModal";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const { getStockData } = useStockCall();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
 
-export default Products
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    getStockData("products");
+  }, []); // eslint-disable-line
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Products
+      </Typography>
+
+      <Button
+        variant="contained"
+        onClick={handleOpen}
+        sx={{ marginBottom: "1rem" }}
+      >
+        New Product
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+
+      <Grid container sx={flex}>
+        {products?.map((product) => (
+          <Grid item key={product.id}>
+            <ProductCard
+              product={product}
+              setOpen={setOpen}
+              setInfo={setInfo}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Products;
