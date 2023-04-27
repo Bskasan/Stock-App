a15a574eb4b8f811e57de56bf5e1d6e91f0b8201
Bskasan <b.kasan@yandex.com>
2023-04-28 00:02:20 +0300

    charts done

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,16 +1,27 @@
 import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
-import React from "react";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
-import ShoppingIcon from "@mui/icons-material/ShoppingCart";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
-import { deepPurple, pink, amber } from "@mui/material/colors";
+import { amber, deepPurple, pink } from "@mui/material/colors";
+import { useSelector } from "react-redux";
 
 const KpiCards = () => {
+  const { sales, purchases } = useSelector((state) => state.stock);
+
+  const totalSales = sales
+    .map((item) => Number(item.price_total))
+    .reduce((acc, val) => acc + val, 0);
+
+  const totalPurchases = purchases
+    .map((item) => Number(item.price_total))
+    .reduce((acc, val) => acc + val, 0);
+
+  const totalProfit = totalSales - totalPurchases;
   const data = [
     {
       id: 1,
       title: "sales",
-      value: "$20000",
+      value: `$${totalSales}`,
       icon: <MonetizationOnIcon sx={{ fontSize: "2.3rem" }} />,
       color: deepPurple[600],
       bgColor: deepPurple[100],
@@ -18,15 +29,15 @@ const KpiCards = () => {
     {
       id: 2,
       title: "profit",
-      value: "$30000",
-      icon: <ShoppingIcon sx={{ fontSize: "2.3rem" }} />,
+      value: `$${totalProfit}`,
+      icon: <ShoppingCartIcon sx={{ fontSize: "2.3rem" }} />,
       color: pink[600],
       bgColor: pink[100],
     },
     {
       id: 3,
       title: "purchases",
-      value: "$10000",
+      value: `$${totalPurchases}`,
       icon: <PaymentsIcon sx={{ fontSize: "2.3rem" }} />,
       color: amber[600],
       bgColor: amber[100],
@@ -34,7 +45,7 @@ const KpiCards = () => {
   ];
 
   return (
-    <Grid container justifyContent={"center"} spacing={5}>
+    <Grid container justifyContent={"center"} spacing={3}>
       {data.map((item) => (
         <Grid
           item
@@ -46,17 +57,19 @@ const KpiCards = () => {
           sx={{ minWidth: "250px" }}
         >
           <Paper sx={{ p: 2 }} elevation={10}>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 5 }}>
               <Avatar
                 sx={{
                   backgroundColor: item.bgColor,
                   color: item.color,
-                  width: "60px",
-                  height: "60px",
+                  width: 70,
+                  height: 70,
+                  ml: 4,
                 }}
               >
                 {item.icon}
               </Avatar>
+
               <Box>
                 <Typography variant="button">{item.title}</Typography>
                 <Typography variant="h4">{item.value}</Typography>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import { Typography } from "@mui/material";
 import KpiCards from "../components/KpiCards";
 import Charts from "../components/Charts";
+import { useEffect } from "react";
+import useStockCall from "../hooks/useStockCall";
 
 const Home = () => {
+  const { getStockData } = useStockCall();
+
+  useEffect(() => {
+    getStockData("sales");
+    getStockData("purchases");
+    
+  }, []);
+
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
